4fc58adad556269fe88df8bd33819f323433b1ac
Muhammad Hammad <hammadmunawarpatel@gmail.com>
2022-05-06 21:13:11 +0200

    Cleaned - Final with all descriptives

--- main_script.R
+++ main_script.R
@@ -1,15 +1,26 @@
-library('tidyverse')
-library('tidyselect')
-library("ggplot2")
-library("dplyr")
-library("corrplot")
-library("reshape2")
-library("data.table")
-library("caret")
-library('reshape2')
+# === PREPARE SYSTEM
+# set language to English
+Sys.setenv(LANG = "en")
 
-# Overview of SECOM dataset
-# Getting SECOM dataset
+# Checking if pacman library installed or not
+if (!require("pacman")) install.packages("pacman")
+
+# load pacman
+library(pacman)
+
+
+p_load('tidyverse')
+p_load('tidyselect')
+p_load("ggplot2")
+p_load("dplyr")
+p_load("corrplot")
+p_load("reshape2")
+p_load("data.table")
+p_load("caret")
+p_load('reshape2')
+
+
+# Importing SECOM dataset - Directly from Online Repository
 secom.data<-read.table("https://archive.ics.uci.edu/ml/machine-learning-databases/secom/secom.data")
 secom.label<-read.table("https://archive.ics.uci.edu/ml/machine-learning-databases/secom/secom_labels.data")
 colnames(secom.data)<-paste("Feature", 1:ncol(secom.data), sep = "_")
@@ -21,14 +32,16 @@ nrow(secom)
 #we create an empty data frame to run our loop to identify
 #NA values for each Feature, we will add as well mean, median
 #first quartile, third quartile, std.dev., percentage of 
-#NA'S per feature and unique values per feature
+#NA'S per feature, unique values per feature, Outliers by 3s, and Influential Values
+
+# Creating empty datafram to append all descriptives
 final_column_descriptives <- data.frame()
 
 
-for(column_name in colnames(secom)){
-  
+for(column_name in colnames(secom.data)) {
+  #column_name = 'Feature_39'
   # Selecting Only the current column in a loop
-  selected_col <- subset(secom, select = column_name)
+  selected_col <- subset(secom.data, select = column_name)
   # Convert column dataframe to vector
   selected_col <- selected_col[, column_name]
   # Count total NA's of the selected column
@@ -44,7 +57,7 @@ for(column_name in colnames(secom)){
   }
   
   
-# Descriptives (mean, median, stdve, q1, q3, 3s rules
+# Calculating Descriptives (mean, median, stdve, q1, q3)
   if(is.numeric(selected_col)){
     Mean <- mean(selected_col,na.rm = T)
     Mean <- round(Mean, digits = 2)
@@ -63,83 +76,87 @@ for(column_name in colnames(secom)){
     Q1 <- NA
     Q3 <- NA
   }
-
-  # Finding outliers in columns
-  Total_Outliers_3s <- (selected_col > Mean + (3 * Standard_Deviation)) | (selected_col < Mean - (3 * Standard_Deviation))
-  Total_Outliers_3s <- sum(Total_Outliers_3s, na.rm = T)
   
   # Finding unique values in each column
   Unique_Values <- length(unique(na.omit(selected_col)))
   
-  combined_descrptive <- data.frame(column_name, SUM_NA, Percentage_NA, Mean, Median, Standard_Deviation, Q1,Q3, Total_Outliers_3s, Unique_Values)
+  
+  # Normalizingthe selected Column
+  range01 <- function(x, ...){(x - min(x, ...)) / (max(x, ...) - min(x, ...))}
+  
+  selected_col_norm <- range01(selected_col, na.rm = T)
+  
+  # Finding standard deviation from the Normalized Column
+  Standard_Deviation_Norm <- sd(selected_col_norm, na.rm = T)
+  
+  Standard_Deviation_Norm <- round(Standard_Deviation_Norm, digits = 3)
+  
+  # Scale the selected column
+  selected_col_scaled <- scale(selected_col)
+  
+  
+  # Finding outliers in the scaled column
+  Total_Outliers_3s <- (selected_col_scaled > 3) | (selected_col_scaled < -3)
+  
+  Total_Outliers_3s <- sum(Total_Outliers_3s, na.rm = T)
+  
+  # Finding Influential Values in the scaled column
+  Total_Influential <- (selected_col_scaled > 2 & selected_col_scaled < 3) | (selected_col_scaled < -2 & selected_col_scaled > -3)
+  
+  Total_Influential <- sum(Total_Influential, na.rm = T)
+  
+  combined_descrptive <- data.frame(column_name, SUM_NA, Percentage_NA, Mean, Median, Standard_Deviation, Q1,Q3, Standard_Deviation_Norm,Total_Outliers_3s,Total_Influential, Unique_Values)
   final_column_descriptives <- rbind(final_column_descriptives,combined_descrptive)
   
-}
+  }
 
-# Finding Duplicate Rows in the Dataframe
-
-c <- secom[!duplicated(as.list(secom))]
+# Finding Duplicate Colummns in the Dataframe
+#c <- secom[!duplicated(as.list(secom))]
 duplicate_columns <- ncol(secom) - ncol(secom[!duplicated(as.list(secom))])
 print('Number of Duplicate columns')
 print(duplicate_columns)
 
 
-library(digest)
-check <- secom[!duplicated(lapply(secom, digest))]
-
-duplicated(t(testframe))
+# Finding columns with constant values i.e only single unique values
+sum(final_column_descriptives$Unique_Values == 1)
 
 
-
-# Standard Deviation Histogram
-sd_his <- final_column_descriptives$Standard_Deviation
-
-sd_his <- sd_his[!is.na(sd_his)]
-
-fun <- dnorm(x2, mean = mean(x), sd = sd(x))
-
-hist(sd_his, prob = TRUE, ylim = c(0, max(fun)),
-     main = "Histogram with density curve")
-lines(density(sd_his), col = 4, lwd = 2)
 # Correlation Matrix
 
-
+# Create a correlation matrix - Using only pairs that are complete, i.e no missing values
 corr_mat <- round(cor(secom.data, use = 'pairwise.complete.obs'),2)
 
-check <- corr_mat %>%                               # start from the correlation matrix
-  as.table() %>% as.data.frame() %>%       # Marek's answer in TidyVerse format
-  subset(Var1 != Var2) %>% # omit diagonal and keep significant correlations (optional...)
+
+# Create a list of unique column combinations with their correlation from the matrix. Diagonals removed and duplicates combinations removed.
+all_comb_correlations <- corr_mat %>%                               
+  as.table() %>% as.data.frame() %>%       
+  subset(Var1 != Var2) %>% 
   filter(!duplicated(paste0(pmax(as.character(Var1), as.character(Var2)), pmin(as.character(Var1), as.character(Var2))))) %>%
-  # keep only unique occurrences, as.character because Var1 and Var2 are factors
-  arrange(desc(Freq))                      # sort by Freq
-
-#corr_mat[(abs(corr_mat) < 0.6) 
+  arrange(desc(Freq))                     
 
 
-# Weakly correlated
-weak_corr <- check %>% filter((Freq > 0.3 & Freq  <= 0.5) | (Freq > -0.3 & Freq <= -0.5))
-print((nrow(weak_corr)/ nrow(check)) * 100)
+# Percentage of unique combination for different correlation levels.
+# No Correlation (0-0.3), Weak  (0.3 - 0.5), Moderate  (0.5 - 0.8), Strong (0.8 - <1), Perfect(1)
+
+no_corr <- all_comb_correlations %>% filter((Freq >= 0 & Freq <= 0.3) | (Freq >= -0.3 & Freq <= 0))
+print((nrow(no_corr)/ nrow(all_comb_correlations)) * 100)
+
+weak_corr <- all_comb_correlations %>% filter((Freq >= 0 & Freq <= 0.3) | (Freq >= -0.3 & Freq <= 0))
+print((nrow(weak_corr)/ nrow(all_comb_correlations)) * 100)
+
+mod_corr <- all_comb_correlations %>% filter((Freq >= 0.5 & Freq <= 0.8) | (Freq <= -0.5 & Freq >= -0.8))
+print((nrow(mod_corr)/ nrow(all_comb_correlations)) * 100)
 
 
-weak_corr <- check %>% filter((Freq == 1) | (Freq == -1))
-print((nrow(weak_corr)/ nrow(check)) * 100)
+strong_corr <- all_comb_correlations %>% filter((Freq >= 0.8 & Freq < 1) | (Freq <= -0.8 & Freq > -1))
+print((nrow(strong_corr)/ nrow(all_comb_correlations)) * 100)
 
-#write.csv(check, file = 'check.csv')
+perfect_correlation <- all_comb_correlations %>% filter((Freq == 1) | (Freq == -1))
+print((nrow(perfect_correlation)/ nrow(all_comb_correlations)) * 100)
 
-library(h2o)
-library(lares)
+NA_corr <- all_comb_correlations %>% filter(is.na(Freq))
+print((nrow(NA_corr)/ nrow(all_comb_correlations)) * 100)
 
-
-corr_cross(cor_data,
-           max_pvalue = 0.05, 
-           top = 10  
-)
-# The SECOM dataset includes 1567 rows (observations) with 590 columns 
-# representing 590 features/signals collected from sensors, together with 
-# the labels representing pass (-1) / fail (1) yield for in house line testing 
-# and associated date time stamp.  
-
-secom.status<-data.frame(table(secom$Status,dnn = c("Status")))
 
 
 #Plotting histograms
@@ -173,16 +190,35 @@ hist((final_column_descriptives$Standard_Deviation),
 #histogram of outliers 
 hist((final_column_descriptives$Total_Outliers_3s),
      labels = TRUE, 
-     ylim = c(0,25), 
-     breaks = 50, 
-     xlim = c(0,1700), 
+     #ylim = c(0,25), 
+     #breaks = 50, 
+     #xlim = c(0,1700), 
      main = "Total number of Outliers based on 3s Rule", 
      xlab = "Number of Outliers",
      col = ("limegreen"))
 
+#histogram of Influential Values 
+hist((final_column_descriptives$Total_Influential),
+     labels = TRUE, 
+     #ylim = c(0,25), 
+     #breaks = 50, 
+     #xlim = c(0,1700), 
+     main = "Influential Values", 
+     xlab = "Number of Influential Values",
+     col = ("limegreen"))
 
-#count number of features that have 1 unique value
-sum(final_column_descriptives$Unique_Values == 1)
+
+# Histogram of Normalized SD
+final_column_descriptives$Standard_Deviation_Norm[is.na(final_column_descriptives$Standard_Deviation_Norm)] <- 0
+
+hist((final_column_descriptives$Standard_Deviation_Norm),
+     labels = TRUE, 
+     #ylim = c(0,25), 
+     #breaks = 50, 
+     #xlim = c(0,1700), 
+     main = "Volatility in the Features", 
+     xlab = "Standard Deviation of Normalized Feature",
+     col = ("limegreen"))
 
 
 # Bar chart of Frequency of Pass and Fail
@@ -199,10 +235,23 @@ text(secom.barplot.1,
      labels = table(secom$Status), 
      pos = 4)
 
+
+
+
+# The SECOM dataset includes 1567 rows (observations) with 590 columns 
+# representing 590 features/signals collected from sensors, together with 
+# the labels representing pass (-1) / fail (1) yield for in house line testing 
+# and associated date time stamp.  
+
+secom.status<-data.frame(table(secom$Status,dnn = c("Status")))
+secom.status
+
 # Split the dataset with respect to class variables proportions (ratio 14:1)
 ## generates indexes for randomly splitting the data into training and test sets
-
-secom.train_index<-createDataPartition(secom$Status, times = 1,p = 0.8, list = FALSE) # to put 80% of data to training set
+# Setting seed so that the data is replicable.
+set.seed(10)
+# Spliting data - Training Data 80% and Test Data 20%
+secom.train_index<-createDataPartition(secom$Status, times = 1,p = 0.8, list = FALSE)
 ## define the training and test sets by using above index
 secom.training<-secom[secom.train_index,]
 secom.data.train<-secom.training[,-c(1,2)]
